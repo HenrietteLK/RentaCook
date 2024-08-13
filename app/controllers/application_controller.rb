6a98cb280c7b86e3f2080e3b9be74f3b55d5867f
HenrietteLK <lk.henriette@gmail.com>
2024-08-13 17:39:21 +0200

    signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -8,5 +8,13 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address, :email, :phone_number])
+  end
+
+  def after_sign_in_path
+    chefs_path
+  end
+
+  def after_sign_up_path
+    chefs_path
   end
 end
