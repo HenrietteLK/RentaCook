f0fd3a4558149e5acd41ccdba4a0056415590faa
Gaelle Lafargue <gaellelfrgue@gmail.com>
2024-08-16 13:01:56 +0200

    dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,7 @@ class PagesController < ApplicationController
   def pendings
     @pending_bookings_user = current_user.bookings.where(status: 'Pending')
     @pending_bookings_chef = Booking.where(chef: current_user.chefs.first, status: 'Pending')
+    @all_bookings_chef = Booking.where(chef: current_user.chefs.first).where.not(status: 'Pending')
   end
 
   def accept_booking
@@ -24,15 +25,4 @@ class PagesController < ApplicationController
   def dashboard
   end
 
-  def accept_booking
-    @booking = Booking.find(params[:id])
-    @booking.update(status: 'Accepted')
-    redirect_to pendings_path, notice: "Booking accepted."
-  end
-
-  def refuse_booking
-    @booking = Booking.find(params[:id])
-    @booking.update(status: 'Refused')
-    redirect_to pendings_path, notice: "Booking refused."
-  end
 end
